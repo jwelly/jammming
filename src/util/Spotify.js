fa9f9cbe570825cccc55276a4f1ff0dd515cb500
jwelly <64269613+jwelly@users.noreply.github.com>
2020-10-03 17:10:40 +0100

    obtain Spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,30 @@
+const clientId = '';
+const redirectUri = 'http://localhost:3000/';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+            window.location = accessUrl;
+        }
+
+        // check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            let expiresIn = Number(expiresInMatch[1]);
+
+            // The following clears the parameters, allowing us to grab a new access token when it expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+    }
+}
+
+export default Spotify;
