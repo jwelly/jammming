11fb2254cf6b48a1f487da04113a97eade126d2b
jwelly <64269613+jwelly@users.noreply.github.com>
2020-10-10 13:47:02 +0100

    only display searched tracks not already in playlist

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -3,17 +3,30 @@ import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
+    alreadyInPlaylist(track) {
+        let checkTrack = this.props.tracksInPlaylist;
+        if (!checkTrack) {      // check if checkTrack is not undefined
+            return;
+        } else {        // check if track is in the playlist
+            return checkTrack.find((savedTrack) => (savedTrack.id === track.id) ? true : false);
+        }
+    }
+    
     render() {
         return (
             <div className="TrackList">
                 {this.props.tracks.map(track => {
-                    return (
-                        <Track track={track}
-                            key={track.id}
-                            onAdd={this.props.onAdd}
-                            onRemove={this.props.onRemove} 
-                            isRemoval={this.props.isRemoval} />
-                    )
+                    if (!this.alreadyInPlaylist(track)) {   // if track is NOT in playlist, ie false
+                        return (                            // then display it in the search results
+                            <Track track={track}
+                                key={track.id}
+                                onAdd={this.props.onAdd}
+                                onRemove={this.props.onRemove} 
+                                isRemoval={this.props.isRemoval} />
+                        )
+                    } else {
+                        return null;        // if track IS in playlist, true,
+                }                           // then remove it from search results
                 })}
             </div>
         )
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -72,7 +72,8 @@ class App extends React.Component {
           <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}
-              onAdd={this.addTrack} />
+              onAdd={this.addTrack}
+              playlistTracks={this.state.playlistTracks} />
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
